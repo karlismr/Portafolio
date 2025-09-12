04baa0b42e155576bb206146123f98b92976cd69
karlismr <katittaa24@gmail.com>
2025-09-12 09:45:29 -0300

    eee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
             especializada en <span className="text-pink-300">React</span>,{" "}
             <span className="text-pink-300">Next.js</span> y{" "}
             <span className="text-pink-300">TypeScript</span>. Me encanta crear
-            UIs limpias, rápidas y accesibles.
+            UIs limpias, rápidas y accesibleeeeeeeeeeeeees.
           </p>
         </div>
       </section>
