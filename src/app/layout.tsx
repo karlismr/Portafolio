504dfc3e672c949a768bd4d6449f0a973c5b1663
karlismr <katittaa24@gmail.com>
2025-09-24 00:47:58 -0300

    fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,14 @@ export const metadata: Metadata = {
     "Desarrollador Web • React • Next.js • SQL • TypeScript • Python • Django",
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/icon.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", type: "image/x-icon" },
     ],
-    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
+
   openGraph: {
     type: "website",
     url: "/",
